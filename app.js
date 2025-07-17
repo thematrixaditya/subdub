07277714cdaaa92dbcf3f801719a6213033128f8
Aditya Patel <adpatel162@outlook.com>
2025-07-17 09:09:15 +0100

    installed mondodb and setup the connection with mongodb atlas 🚀

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import { PORT } from "./config/env.js";
 import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
+import connectToDatabase from "./database/mongodb.js";
 
 const app = express();
 
@@ -15,8 +16,10 @@ app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+
+  await connectToDatabase();
 });
 
 export default app;
---
+++ database/mongodb.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import { DB_URI, NODE_ENV } from "../config/env.js";
+
+if (!DB_URI) {
+  throw new Error("Database URI is not defined in the environment variables. Please setup DB_URI in your .env.*.local file.");
+}
+
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`🚀 Connected to MongoDB in ${NODE_ENV} mode.`);
+  } catch (error) {
+    console.error("🥲 Error connecting to MongoDB:", error);
+    process.exit(1);
+  }
+}
+
+export default connectToDatabase;
