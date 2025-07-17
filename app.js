1bc768adb00622f8a81b27d32021376c221e8e86
Aditya Patel <adpatel162@outlook.com>
2025-07-17 08:21:52 +0100

    completed the setup for dotenv

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { PORT } from "./config/env.js";
 
 const app = express();
 
@@ -6,8 +7,8 @@ app.get("/", (req, res) => {
   res.send("Welcome to the subscription tracker API!");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 export default app;
